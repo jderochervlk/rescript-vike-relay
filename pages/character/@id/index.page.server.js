c092976c5151772540ef5f633566834f32ca6ba8
Josh Vlk <josh@vlkpack.com>
2023-10-06 13:41:40 -0400

    working

--- pages/character/@id/index.page.server.js
+++ pages/character/@id/index.page.server.js
@@ -1,15 +1,9 @@
-import { makeEnvironment } from '../../../renderer/RelayEnvironment'
-import { makePrerender } from '../../../src/page__character_server.mjs'
-import RelayClientSSR from 'react-relay-network-modern-ssr/lib/client';
-import { window } from 'browser-monads'
 import RelayServerSSR from 'react-relay-network-modern-ssr/lib/server';
+import { makeEnvironment } from '../../../renderer/RelayEnvironment';
+import { makePrerender } from '../../../src/page__character_server.mjs';
 
 export async function prerender() {
-    const relayClientSSR = new RelayClientSSR(window.__RELAY_BOOTSTRAP_DATA__);
     const relayServerSSR = new RelayServerSSR()
 
-    let isClient = typeof window !== undefined
-    return makePrerender(isClient ?
-        makeEnvironment(relayClientSSR)
-        : makeEnvironment(relayServerSSR))
+    return makePrerender(makeEnvironment(relayServerSSR))
 }
